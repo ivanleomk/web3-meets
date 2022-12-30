9db0671c8bce381fec71aff0917ad8cc2c4d68e8
Ivan Leo <ivanleo@Ivans-MacBook-Pro.local>
2022-12-30 16:23:18 +0800

    Fixing up a spinner

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,6 +1,7 @@
 import { CalendarIcon, MapPinIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { ClipLoader } from "react-spinners";
 import Calendar from "../components/Calendar";
 import EventTag from "../components/EventTag";
 import { formatDate, formatDateMonth, formatDateTime } from "../utils/date";
@@ -36,72 +37,79 @@ const Schedule = () => {
           </div>
         </div>
         <ol className="mt-4 divide-y divide-gray-100 overflow-auto text-sm leading-6 lg:col-span-7 xl:col-span-8">
-          {!data
-            ? null
-            : data
-                .filter((event) => event.eventStart > selectedDay)
-                .map((meeting) => (
-                  <li
-                    key={meeting.id}
-                    className="relative grid grid-cols-5 space-x-6 py-6 xl:static"
-                  >
-                    <img
-                      src={meeting.eventImage}
-                      alt=""
-                      className="col-span-2"
-                    />
-                    <div className="col-span-3">
-                      <div className="mb-4">
-                        <h3 className="pr-10 font-semibold text-gray-900 xl:pr-0">
-                          {meeting.title}
-                        </h3>
-                        <div className="mt-2 flex">
-                          <EventTag name={meeting.eventType} />
-                          <EventTag name={meeting.eventCost} />
-                        </div>
+          {!data ? (
+            <div className="flex h-full items-center justify-center">
+              <div className="flex items-center ">
+                <ClipLoader />
+                <p className="ml-4">Downloading the latest list of events</p>
+              </div>
+            </div>
+          ) : (
+            data
+              .filter((event) => event.eventStart > selectedDay)
+              .map((meeting) => (
+                <li
+                  key={meeting.id}
+                  className="relative grid grid-cols-5 space-x-6 py-6 xl:static"
+                >
+                  <img src={meeting.eventImage} alt="" className="col-span-2" />
+                  <div className="col-span-3">
+                    <div className="mb-4">
+                      <h3 className="pr-10 font-semibold text-gray-900 xl:pr-0">
+                        {meeting.title}
+                      </h3>
+                      <div className="mt-2 flex">
+                        <EventTag name={meeting.eventType} />
+                        <EventTag name={meeting.eventCost} />
                       </div>
-                      <dl className="mt-2 flex flex-col text-gray-500 xl:flex-row">
-                        <div className="flex items-start space-x-3">
-                          <dt className="mt-0.5">
-                            <span className="sr-only">Date</span>
-                            <CalendarIcon
-                              className="h-5 w-5 text-gray-400"
-                              aria-hidden="true"
-                            />
-                          </dt>
-                          <dd>
-                            <time dateTime={formatDate(meeting.eventStart)}>
-                              {formatDateMonth(meeting.eventStart)}{" "}
-                              {formatDateTime(meeting.eventStart)} -{" "}
-                              {formatDateMonth(meeting.eventEnd)}{" "}
-                              {formatDateTime(meeting.eventEnd)}
-                            </time>
-                          </dd>
-                        </div>
-                        <div className="mt-2 flex items-start space-x-3 xl:mt-0 xl:ml-3.5 xl:border-l xl:border-gray-400 xl:border-opacity-50 xl:pl-3.5">
-                          <dt className="mt-0.5">
-                            <span className="sr-only">Location</span>
-                            <MapPinIcon
-                              className="h-5 w-5 text-gray-400"
-                              aria-hidden="true"
-                            />
-                          </dt>
-                          <dd>{meeting.location}</dd>
-                        </div>
-                      </dl>
-                      <p className="mt-3 text-gray-500">
-                        {meeting.description.length > 200
-                          ? meeting.description.slice(0, 150) + "..."
-                          : meeting.description}
-                      </p>
                     </div>
-                  </li>
-                ))}
+                    <dl className="mt-2 flex flex-col text-gray-500 xl:flex-row">
+                      <div className="flex items-start space-x-3">
+                        <dt className="mt-0.5">
+                          <span className="sr-only">Date</span>
+                          <CalendarIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        </dt>
+                        <dd>
+                          <time dateTime={formatDate(meeting.eventStart)}>
+                            {formatDateMonth(meeting.eventStart)}{" "}
+                            {formatDateTime(meeting.eventStart)} -{" "}
+                            {formatDateMonth(meeting.eventEnd)}{" "}
+                            {formatDateTime(meeting.eventEnd)}
+                          </time>
+                        </dd>
+                      </div>
+                      <div className="mt-2 flex items-start space-x-3 xl:mt-0 xl:ml-3.5 xl:border-l xl:border-gray-400 xl:border-opacity-50 xl:pl-3.5">
+                        <dt className="mt-0.5">
+                          <span className="sr-only">Location</span>
+                          <MapPinIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        </dt>
+                        <dd>{meeting.location}</dd>
+                      </div>
+                    </dl>
+                    <p className="mt-3 text-gray-500">
+                      {meeting.description.length > 200
+                        ? meeting.description.slice(0, 150) + "..."
+                        : meeting.description}
+                    </p>
+                  </div>
+                </li>
+              ))
+          )}
           {data?.filter((event) => event.eventStart > selectedDay).length ==
           0 ? (
-            <p className="text-center">
-              No events fit the criteria that you&apos;ve set out
-            </p>
+            <div className="flex h-full items-center justify-center">
+              <div className="flex items-center ">
+                <p className="text-center">
+                  No events fit the criteria that you&apos;ve set out
+                </p>
+              </div>
+            </div>
           ) : null}
         </ol>
       </div>
